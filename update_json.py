b21306ae5c9dfe318f31296e6cca95e9e385451a
Balackburn <93828569+Balackburn@users.noreply.github.com>
2023-07-11 21:15:46 +0200

    added old release

--- update_json.py
+++ update_json.py
@@ -2,19 +2,15 @@ import json
 import re
 import requests
 
-# Fetch the latest release information from GitHub
-def fetch_latest_release(repo_url, keyword):
+# Fetch all release information from GitHub
+def fetch_all_releases(repo_url, keyword):
     api_url = f"https://api.github.com/repos/{repo_url}/releases"
     headers = {"Accept": "application/vnd.github+json"}
     response = requests.get(api_url, headers=headers)
     releases = response.json()
     sorted_releases = sorted(releases, key=lambda x: x["published_at"], reverse=True)
 
-    for release in sorted_releases:
-        if keyword in release["name"]:
-            return release
-
-    raise ValueError(f"No release found containing the keyword '{keyword}'.")
+    return [release for release in sorted_releases if keyword in release["name"]]
 
 # Update the JSON file with the fetched data
 def remove_tags(text):
@@ -27,60 +23,55 @@ def update_json_file(json_file, fetched_data):
         data = json.load(file)
 
     app = data["apps"][0]
-    full_version = fetched_data["tag_name"].lstrip('v')
-    tag = fetched_data["tag_name"]
-    version = re.search(r"(\d+\.\d+\.\d+)", full_version).group(1)
-    app["version"] = version
-    app["versionDate"] = fetched_data["published_at"]
+    
+    # Ensure 'versions' key exists in app
+    if "versions" not in app:
+        app["versions"] = []
 
-    description = fetched_data["body"]
-    keyword = "YTLitePlus Release Information"
-    if keyword in description:
-        description = description.split(keyword, 1)[1].strip()
+    for release in fetched_data:
+        full_version = release["tag_name"].lstrip('v')
+        tag = release["tag_name"]
+        version = re.search(r"(\d+\.\d+\.\d+)", full_version).group(1)
+        versionDate = release["published_at"]
 
-    description = remove_tags(description)
-    description = re.sub(r'\*{2}', '', description)
-    description = re.sub(r'-', '•', description)
-    description = re.sub(r'`', '"', description)
+        description = release["body"]
+        keyword = "YTLitePlus Release Information"
+        if keyword in description:
+            description = description.split(keyword, 1)[1].strip()
 
-    app["versionDescription"] = description
-    app["downloadURL"] = fetched_data["assets"][0]["browser_download_url"]
-    app["size"] = fetched_data["assets"][0]["size"]
+        description = remove_tags(description)
+        description = re.sub(r'\*{2}', '', description)
+        description = re.sub(r'-', '•', description)
+        description = re.sub(r'`', '"', description)
 
-    # Ensure 'news' key exists in data
-    if "news" not in data:
-        data["news"] = []
+        downloadURL = release["assets"][0]["browser_download_url"]
+        size = release["assets"][0]["size"]
 
-    # Add news entry if there's a new release
-    news_identifier = f"release-{full_version}"
-    news_entry = {
-        "title": f"{full_version}",
-        "identifier": news_identifier,
-        "caption": f"Update of YTLitePlus just got released!",
-        "date": fetched_data["published_at"],
-        "tintColor": "#000000",
-        "imageURL": "https://raw.githubusercontent.com/Balackburn/YTLitePlusAltstore/main/screenshots/news/new_release.png",
-        "notify": True,
-        "url": f"https://github.com/Balackburn/YTLitePlus/releases/tag/{tag}"
-    }
+        version_entry = {
+            "version": version,
+            "date": versionDate,
+            "description": description,
+            "downloadURL": downloadURL,
+            "size": size
+        }
 
-    # Check if the news entry already exists
-    news_entry_exists = any(item["identifier"] == news_identifier for item in data["news"])
+        # Check if the version entry already exists
+        version_entry_exists = any(item["version"] == version for item in app["versions"])
 
-    # Add the news entry if it doesn't exist
-    if not news_entry_exists:
-        data["news"].append(news_entry)
+        # Add the version entry if it doesn't exist
+        if not version_entry_exists:
+            app["versions"].append(version_entry)
 
     with open(json_file, "w") as file:
         json.dump(data, file, indent=2)
-        
+
 # Main function
 def main():
     repo_url = "Balackburn/YTLitePlus"
     json_file = "apps.json"
     keyword = "YTLitePlus"
 
-    fetched_data = fetch_latest_release(repo_url, keyword)
+    fetched_data = fetch_all_releases(repo_url, keyword)
     update_json_file(json_file, fetched_data)
 
 if __name__ == "__main__":
